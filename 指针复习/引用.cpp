2c851756622eb7aca882fbce512242799d341ca7
zhjwang <695331215@qq.com>
2021-03-07 22:16:00 +0800

    指针复习

--- 指针复习/引用.cpp
+++ 指针复习/引用.cpp
@@ -34,8 +34,62 @@ void test2()
         printf("%d\n", test[i]);
     }
 }
+//函数返回引用
+int &test3()
+{
+    //不能返回栈变量
+    int num = 10;
+    return num; //函数返回谁，引用就是该变量的别名
+}
+
+int &test4()
+{
+    //可以返回static的变量
+    static int num = 10;
+    return num;
+}
+
+//当函数作为左值是，必须返回引用
+//你看下面的调用：test5() = 00; 如果说test5()返回的是一个具体的数字的话，就出错了，数字是不能放在=的左边的。
+//而返回的是引用的时候，就是一个变量的别名，当然是可以作左值的。
+int &test5()
+{
+    static int num = 10;
+    std::cout << num << std::endl;
+    return num;
+}
+
+
 int main()
 {
-    test2();
+
+    test5() = 00;  //把00赋值给test5()函数的返回值
+    test5();//0
+    // int &ret = test3(); //ret是num的别名，因为是栈变量，已经释放，别名指向一个不确定的空间
+    // std::cout << ret << std::endl;
+
+    // int &ret1 = test4();
+    // std::cout << ret1 << std::endl;
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
